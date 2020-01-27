d61b8dfe6cc62b481c39f460ce9f4507f5c5b504
Emanuele <emanuele.luzio@gmail.com>
2020-01-27 20:05:26 -0300

    naive backtest added

---
+++ portfolio_analyzer/backtest.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class NaiveBackTest:
+    """Performs naive backtest of the portfolio."""
+
+    def __init__(self, tickers_ratio, data):
+        self.tickers_ratio = tickers_ratio
+        self.tickers = tickers_ratio.keys()
+        self.weights = tickers_ratio.values()
+        self.data = data
+
+    def run(self, capital=100.0):
+        """Execute the back test."""
+        log_data = np.log(
+            np.sum(
+                (1 + self.data[self.tickers].pct_change()).dropna() * self.weights,
+                axis=1,
+            )
+        )
+        historical_series = np.cumsum(log_data)
+        self.data["portfolio"] = np.exp(historical_series) * capital
+        return self.data
